bdc92bdd1a22cb366e6992e4120b7d8a9e3ffaf7
Alexei Kornev <alex1234562557@gmail.com>
2022-10-28 22:25:22 +0700

    add IStorage interface

---
+++ Storage/IStorage.cs
@@ -0,0 +1,10 @@
+namespace webapp.Storage;
+
+public interface IStorage<T> where T : class
+{
+    List<T> All { get; }
+    T this[Guid id] { get; set; }
+    void Add(T value);
+    void RemoveAt(Guid id);
+    bool Has(Guid id);
+}
