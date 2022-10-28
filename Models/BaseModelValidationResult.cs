8d90d51e742a7e5ffc1b390ebcb0abb4ded3265f
Alexei Kornev <alex1234562557@gmail.com>
2022-10-28 22:27:48 +0700

    add validation

---
+++ Models/BaseModelValidationResult.cs
@@ -0,0 +1,17 @@
+using System.Text;
+
+namespace webapp.Models;
+
+public class BaseModelValidationResult
+{
+    private StringBuilder _errorBuilder = new StringBuilder();
+
+    public bool IsValid { get; private set; } = true;
+    public string Errors { get => _errorBuilder.ToString().Trim(); }
+
+    public void Append(string error)
+    {
+        IsValid = false;
+        _errorBuilder.AppendLine(error);
+    }
+}
--- Models/Place.cs
+++ Models/Place.cs
@@ -1,14 +1,27 @@
-namespace webapp;
+namespace webapp.Models;
 
 public class Place
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-
     public string? Name { get; set; }
-
     public string? Category { get; set; }
-
     public float Latitude { get; set; }
-
     public float Longitude { get; set; }
+
+    public BaseModelValidationResult Validate()
+    {
+        var validationResult = new BaseModelValidationResult();
+
+        if (string.IsNullOrWhiteSpace(Name)) validationResult.Append($"Name cannot be empty");
+        if (string.IsNullOrWhiteSpace(Category)) validationResult.Append($"Category cannot be empty");
+        if (!(Latitude >= -90 && Latitude <= 90)) validationResult.Append($"Latitude {Latitude} is out of range (-90..90)");
+        if (!(Longitude >= -90 && Longitude <= 90)) validationResult.Append($"Longitude {Longitude} is out of range (-90..90)");
+        
+        return validationResult;
+    }
+
+    public override string ToString()
+    {
+        return $"{Category} {Name} at {Latitude} {Longitude}";
+    }
 }
