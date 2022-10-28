38e6bb27674e3b10540e1f8b4c9c9b16ddba53b9
Alexei Kornev <alex1234562557@gmail.com>
2022-10-28 22:49:56 +0700

    add version controlling get request

---
+++ Models/Version.cs
@@ -0,0 +1,8 @@
+namespace webapp.Models;
+
+public class Version
+{
+    public string Company { get; set; }
+    public string ProductVersion { get; set; }
+    public string Product { get; set; }
+}
