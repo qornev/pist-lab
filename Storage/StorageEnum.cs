ce647a9636ab4a3c888d2dcdc5e1c9f8193f1c45
qornev <alex1234562557@gmail.com>
2022-11-19 16:22:40 +0700

    add resolve class storage in DI config with choice in appsetting.json

---
+++ Storage/StorageEnum.cs
@@ -0,0 +1,26 @@
+public enum StorageEnum
+{
+    Undefined,
+    MemCache,
+    FileStorage
+}
+
+public static class StorageEnumExtensions
+{
+    public static StorageEnum ToStorageEnum(this string value)
+    {
+        switch (value)
+        {
+            case var s when s.ToLowerInvariant() == "memcache"
+                            || s.ToLowerInvariant() == "cache"
+                            || s.ToLowerInvariant() == "mem":
+                return StorageEnum.MemCache;
+            case var s when s.ToLowerInvariant() == "filestorage"
+                            || s.ToLowerInvariant() == "file"
+                            || s.ToLowerInvariant() == "storage":
+                return StorageEnum.FileStorage;
+            default:
+                return StorageEnum.Undefined;
+        }
+    }
+}
